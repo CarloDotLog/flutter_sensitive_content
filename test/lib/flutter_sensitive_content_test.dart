69d5d259cf6cfbcc277fc88b21fcdc77301e37f4
carlo.loguercio <carlo.log@gmail.com>
2022-10-31 11:22:34 +0100

    fix: flutter format

--- test/lib/flutter_sensitive_content_test.dart
+++ test/lib/flutter_sensitive_content_test.dart
@@ -4,28 +4,28 @@ import '../../example/lib/main.dart';
 
 void main() {
   testWidgets('AppLifecycleStateChanged smoke test',
-          (WidgetTester tester) async {
-        // Build our app and trigger a frame.
-        await tester.pumpWidget(const MyApp());
+      (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
 
-        // Verify that 'Sensitive Data' is printed at start.
-        expect(find.text('Sensitive Data'), findsOneWidget);
-        expect(find.text('Public Data'), findsNothing);
+    // Verify that 'Sensitive Data' is printed at start.
+    expect(find.text('Sensitive Data'), findsOneWidget);
+    expect(find.text('Public Data'), findsNothing);
 
-        // change AppLifecycleState to AppLifecycleState.paused
-        tester.binding.handleAppLifecycleStateChanged(AppLifecycleState.paused);
-        await tester.pump();
+    // change AppLifecycleState to AppLifecycleState.paused
+    tester.binding.handleAppLifecycleStateChanged(AppLifecycleState.paused);
+    await tester.pump();
 
-        // Verify that 'Public Data' is printed on AppLifecycleState.paused.
-        expect(find.text('Sensitive Data'), findsNothing);
-        expect(find.text('Public Data'), findsOneWidget);
+    // Verify that 'Public Data' is printed on AppLifecycleState.paused.
+    expect(find.text('Sensitive Data'), findsNothing);
+    expect(find.text('Public Data'), findsOneWidget);
 
-        // change AppLifecycleState to AppLifecycleState.resumed
-        tester.binding.handleAppLifecycleStateChanged(AppLifecycleState.resumed);
-        await tester.pump();
+    // change AppLifecycleState to AppLifecycleState.resumed
+    tester.binding.handleAppLifecycleStateChanged(AppLifecycleState.resumed);
+    await tester.pump();
 
-        // Verify that 'Sensitive Data' is printed at start.
-        expect(find.text('Sensitive Data'), findsOneWidget);
-        expect(find.text('Public Data'), findsNothing);
-      });
+    // Verify that 'Sensitive Data' is printed at start.
+    expect(find.text('Sensitive Data'), findsOneWidget);
+    expect(find.text('Public Data'), findsNothing);
+  });
 }
